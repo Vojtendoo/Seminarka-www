966b312f778a6c5f6e26ef83fb4ebe999f260f15
Vojtendoo <vojta.fous@gmail.com>
2024-12-13 12:50:27 +0100

    Delete seminarka www - testing directory

--- seminarka www - testing/hamburger_menu.js
+++
@@ -1,8 +0,0 @@
-// Výběr prvků
-const hamburger = document.querySelector('.hamburger');
-const sideMenu = document.querySelector('.side-menu');
-
-// Přidání události kliknutí na hamburger
-hamburger.addEventListener('click', () => {
-    sideMenu.classList.toggle('active');
-});
